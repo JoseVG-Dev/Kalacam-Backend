f16b383c4fbd5cbf3981997a689ff1b1c5ab1506
joseDev <josevelasquezguerrerroo@gmail.com>
2025-10-19 16:47:49 -0300

    env changes

--- database.py
+++ database.py
@@ -7,13 +7,7 @@ import os
 load_dotenv()
 
 
-USER = os.getenv("user")
-PASSWORD = os.getenv("password")
-HOST = os.getenv("host")
-PORT = os.getenv("port")
-DBNAME = os.getenv("dbname")
-
-DATABASE_URL = f"postgresql+psycopg2://{USER}:{PASSWORD}@{HOST}:{PORT}/{DBNAME}?sslmode=require"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(
     DATABASE_URL,
